d6a133ea4a275931c5cd99fbab7b408ffee82db0
Clarence Chan <programclarence@gmail.com>
2017-05-27 18:30:03 -0400

    added a searchbox to search-icon

--- client/src/Search.js
+++ client/src/Search.js
@@ -1,9 +1,40 @@
 import React, { Component } from 'react';
 
 class Search extends Component {
+  constructor(props){
+    super(props);
+  
+  this.state = {
+    search: "",
+    searchOpen: false
+  }
+  this.handleClick = this.handleClick.bind(this);
+  this.handleChange = this.handleChange.bind(this);
+  }
+  handleClick (event) {
+    return this.setState({
+      searchOpen: !this.state.searchOpen
+    });
+  }
+  handleChange(event) {
+    let value = event.target.value;
+    
+    return this.setState({
+      search: value
+    });
+  }
   render() {
     return (
-      <a href="#" className="search-icon"><i className="fa fa-search"></i></a> 
+      <div className="search-container">
+        <a onClick={this.handleClick} href="#" className="search-icon"><i className="fa fa-search"></i></a> 
+        <input 
+          disabled={!this.state.searchOpen} 
+          onChange={this.handleChange} 
+          className={this.state.searchOpen ? "expanded":null} 
+          type="search" 
+          placeholder="Search..."
+        />
+      </div>
     );
   }
 }
