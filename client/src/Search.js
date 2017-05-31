0ada4bf597fcc98b02170d3413a073725da0c2b8
Clarence Chan <programclarence@gmail.com>
2017-05-31 13:19:08 -0400

    moved search function to App.js

--- client/src/Search.js
+++ client/src/Search.js
@@ -8,15 +8,11 @@ class Search extends Component {
       searchValue: "",
       searchOpen: false
     }
-
     this.handleSearchClick = this.handleSearchClick.bind(this);
   }
   handleSearchClick(event) {
-    const resetSearch = () => this.setState({ searchOpen: false, searchValue: "" });
     if (this.state.searchOpen && this.state.searchValue) {
-      // Fetch Search
-      console.log(`Do Search for ${this.state.searchValue}`);
-      resetSearch();
+      this.props.handleSearch(this.state.searchValue);
     } else {
       this.setState({ searchOpen: !this.state.searchOpen })
     }
@@ -27,7 +23,7 @@ class Search extends Component {
         <a onClick={this.handleSearchClick} href="#" className="search-icon"><i className="fa fa-search"></i></a>
         <input
           disabled={!this.state.searchOpen}
-          onKeyUp={event=>{(event.key ==="Enter")&&this.handleSearchClick()}}
+          onKeyUp={event => { (event.key === "Enter") && this.handleSearchClick() }}
           value={this.state.searchValue}
           onChange={event => this.setState({ searchValue: event.target.value })}
           className={this.state.searchOpen ? "expanded" : null}
