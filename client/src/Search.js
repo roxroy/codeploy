081285a23b820cc4f4caa74483a3e67575864172
Clarence Chan <programclarence@gmail.com>
2017-05-31 12:36:13 -0400

    added search event handlers

--- client/src/Search.js
+++ client/src/Search.js
@@ -1,25 +1,37 @@
 import React, { Component } from 'react';
 
 class Search extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-  
+
     this.state = {
       searchValue: "",
       searchOpen: false
     }
+
+    this.handleSearchClick = this.handleSearchClick.bind(this);
+  }
+  handleSearchClick(event) {
+    const resetSearch = () => this.setState({ searchOpen: false, searchValue: "" });
+    if (this.state.searchOpen && this.state.searchValue) {
+      // Fetch Search
+      console.log(`Do Search for ${this.state.searchValue}`);
+      resetSearch();
+    } else {
+      this.setState({ searchOpen: !this.state.searchOpen })
+    }
   }
   render() {
-    console.log(this.state.searchValue);
     return (
       <div className="search-container">
-        <a onClick={event => this.setState({searchOpen: !this.state.searchOpen})} href="#" className="search-icon"><i className="fa fa-search"></i></a> 
-        <input 
+        <a onClick={this.handleSearchClick} href="#" className="search-icon"><i className="fa fa-search"></i></a>
+        <input
           disabled={!this.state.searchOpen}
+          onKeyUp={event=>{(event.key ==="Enter")&&this.handleSearchClick()}}
           value={this.state.searchValue}
-          onChange={event => this.setState({searchValue: event.target.value})}
-          className={this.state.searchOpen ? "expanded" : null} 
-          type="search" 
+          onChange={event => this.setState({ searchValue: event.target.value })}
+          className={this.state.searchOpen ? "expanded" : null}
+          type="search"
           placeholder="Search..."
         />
       </div>
