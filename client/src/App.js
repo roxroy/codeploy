7883e4309a700f8f53ae379fc5990e5513e1cd1e
Clarence Chan <programclarence@gmail.com>
2017-06-03 18:38:08 -0400

    search filter implemented

--- client/src/App.js
+++ client/src/App.js
@@ -5,16 +5,18 @@ const SortButton = require('./SortButton');
 const Resources = require('./Resources');
 const MyJobs = require('./MyJobs');
 
-// TODO: dummy data, delete to test fetching
-const loremipsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed gravida est sit amet mi egestas, a pharetra sem hendrerit. Ut sit amet lacinia ex, vel pellentesque metus. In placerat, lacus eget porttitor imperdiet, sem nibh faucibus turpis, ultricies ultricies turpis orci in augue. Integer ut posuere ante. Pellentesque blandit purus at tortor malesuada porttitor venenatis sed lacus.";
-var resources = [{ "image": "https://www.sololearn.com/Icons/Courses/1024.png", "url": "https://www.website1.com/", "addedBy": "user1", "name": "abcd", "date": "01/03/2016", "rating": "1/5", "golds": "1", "description": loremipsum },
-{ "image": "https://image.flaticon.com/teams/new/1-freepik.jpg", "url": "https://www.website2.com/", "addedBy": "user2", "name": "aaab", "date": "02/14/2017", "rating": "2/5", "golds": "2", "description": loremipsum },
-{ "image": "http://www.freeiconspng.com/uploads/flat-mac-icon-15.png", "url": "https://www.website3.com/", "addedBy": "user3", "name": "name3", "date": "01/01/2017", "rating": "3/5", "golds": "3", "description": loremipsum }];
-
-
 class App extends Component {
   constructor(props) {
     super(props);
+
+    // TODO: dummy data, delete to test fetching
+    let loremipsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed gravida est sit amet mi egestas, a pharetra sem hendrerit. Ut sit amet lacinia ex, vel pellentesque metus. In placerat, lacus eget porttitor imperdiet, sem nibh faucibus turpis, ultricies ultricies turpis orci in augue. Integer ut posuere ante. Pellentesque blandit purus at tortor malesuada porttitor venenatis sed lacus.";
+    this.resources = [{ "image": "https://www.sololearn.com/Icons/Courses/1024.png", "url": "https://www.website1.com/", "addedBy": "user1", "name": "abcd", "date": "01/03/2016", "rating": "1/5", "golds": "1", "description": loremipsum },
+    { "image": "https://image.flaticon.com/teams/new/1-freepik.jpg", "url": "https://www.website2.com/", "addedBy": "user2", "name": "aaab", "date": "02/14/2017", "rating": "2/5", "golds": "2", "description": loremipsum },
+    { "image": "http://www.freeiconspng.com/uploads/flat-mac-icon-15.png", "url": "https://www.website3.com/", "addedBy": "user3", "name": "name3", "date": "01/01/2017", "rating": "3/5", "golds": "3", "description": loremipsum }];
+
+    // TODO: move to assign after fetch
+    this.globalResources = this.resources;
 
     this.state = {
       fetching: true,
@@ -131,8 +133,8 @@ class App extends Component {
     let re = value;
     re = new RegExp(re, "im");
 
-    if (!resources) return;
-    for (let cObj of resources) {
+    if (!this.globalResources) return;
+    for (let cObj of this.globalResources) {
       if (re.test(cObj.name) || re.test(cObj.addedBy) || re.test(cObj.description)) searchedArray.push(cObj);
     }
     if (searchedArray.length === 0) {
@@ -140,6 +142,10 @@ class App extends Component {
     } else {
       console.log("Found: ", searchedArray);
     }
+
+    // todo: change when fetching resources is implemented
+    // change variable assign to state.resource assign
+    this.resources = searchedArray;
     this.setState({
       viewingJobs: false
     });
@@ -172,7 +178,7 @@ class App extends Component {
                   : this.state.resources}
               </p>
               <SortButton sortResources={this.sortResources} />
-              <Resources resources={resources} sortByDate={this.state.sortByDate} />
+              <Resources resources={this.resources} sortByDate={this.state.sortByDate} />
             </div>
           )
         }
