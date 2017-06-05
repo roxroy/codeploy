060b19aceebeb38b000301598f4f4b27b0b6366d
Clarence Chan <programclarence@gmail.com>
2017-06-05 17:56:15 -0400

    added fa-arrows to resource tables based on currentSort

--- client/src/ResourceTable.js
+++ client/src/ResourceTable.js
@@ -5,19 +5,29 @@ function ResourceTable(props) {
 
   let arrowD = props.currentSort[1] ?
     <i className="fa fa-arrow-up" aria-hidden="true"></i> :
-    <i className="fa fa-arrow-up" aria-hidden="true"></i>;
-
+    <i className="fa fa-arrow-down" aria-hidden="true"></i>;
+    
   return (
     <div className="resource-table">
       <table>
         <tbody>
           <tr>
             <th className="resourceName" onClick={props.handleSort} colSpan="2">
-              Resource Name
-              </th>
-            <th className="dateAdded" onClick={props.handleSort}>Date Added</th>
-            <th className="rating" onClick={props.handleSort}>Rating</th>
-            <th className="golds" onClick={props.handleSort}>Gold Stars</th>
+              {"Resource Name "}
+              {(props.currentSort[0] === "resourceName") && arrowD}
+            </th>
+            <th className="dateAdded" onClick={props.handleSort}>
+              {"Date Added "}
+              {(props.currentSort[0] === "dateAdded") && arrowD}
+            </th>
+            <th className="rating" onClick={props.handleSort}>
+              {"Rating "}
+              {(props.currentSort[0] === "rating") && arrowD}
+            </th>
+            <th className="golds" onClick={props.handleSort}>
+              {"Gold Stars "}
+              {(props.currentSort[0] === "golds") && arrowD}
+            </th>
             {(props.fromJobModal) ? <th>Link to Resource</th> : <th>Additional Information</th>}
           </tr>
           {props.resources.map((row, i) => <ResourceRow
