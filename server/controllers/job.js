aa05b9d5ded477ecdb1ae79966129e36954f306b
Unknown <roxroy@devicode.com>
2017-06-11 18:35:05 -0700

    Convert date felds to Date type.

--- server/controllers/job.js
+++ server/controllers/job.js
@@ -1,21 +1,27 @@
 let Jobs = require('../models/job');
 
+const makeDate = (daysAdjustment) => {
+	let currentTime = new Date();
+	currentTime.setDate(currentTime.getDate()+daysAdjustment);
+	return currentTime;
+}
 
 module.exports.all = (req, res) => {
 	let username = "";
 	if (req.user)
 		username = req.user.username;
 
+
 	const myResources = [
-		{ "image": "https://www.sololearn.com/Icons/Courses/1024.png", "url": "https://www.website1.com/", "addedBy": "user1", "name": "abcd", "date": "01/03/2016", "rating": "1/5", "golds": "1" },
-		{ "image": "https://www.sololearn.com/Icons/Courses/1024.png", "url": "https://www.website2.com/", "addedBy": "user2", "name": "aaab", "date": "02/14/2017", "rating": "2/5", "golds": "2" },
-		{ "image": "https://www.sololearn.com/Icons/Courses/1024.png", "url": "https://www.website3.com/", "addedBy": "user3", "name": "name3", "date": "01/01/2017", "rating": "3/5", "golds": "3" },
+		{ "image": "https://www.sololearn.com/Icons/Courses/1024.png", "url": "https://www.website1.com/", "addedBy": "user1", "name": "abcd", "date": makeDate(-2), "rating": "1/5", "golds": "1" },
+		{ "image": "https://www.sololearn.com/Icons/Courses/1024.png", "url": "https://www.website2.com/", "addedBy": "user2", "name": "aaab", "date": makeDate(-100), "rating": "2/5", "golds": "2" },
+		{ "image": "https://www.sololearn.com/Icons/Courses/1024.png", "url": "https://www.website3.com/", "addedBy": "user3", "name": "name3", "date": makeDate(-80), "rating": "3/5", "golds": "3" },
 	];
 	let myJobs = [
-		{ "jobPosition": "position 1", "companyName": "Widget name1", "addedBy": username, "dateApplied": "01/03/2014", "resources": myResources, "comments": "sent thank you note. received no response." },
-		{ "jobPosition": "position 2", "companyName": "ABC name2", "addedBy": username,"dateApplied": "02/02/2015", "resources": myResources, "comments": "sent thank you note. received no response." },
-		{ "jobPosition": "position 3", "companyName": "Net name3 ", "addedBy": username, "dateApplied": "03/01/2016", "resources": myResources, "comments": "sent thank you note. received no response." },
-		{ "jobPosition": "position 4", "companyName": "Carter",  "addedBy": username, "dateApplied": "05/31/2017", "resources": myResources, "comments": "Sent references. Waiting on reply." },
+		{ "jobPosition": "position 1", "companyName": "Widget name1", "addedBy": username, "dateApplied": makeDate(-2), "resources": myResources, "comments": "sent thank you note. received no response." },
+		{ "jobPosition": "position 2", "companyName": "ABC name2", "addedBy": username,"dateApplied": makeDate(-132), "resources": myResources, "comments": "sent thank you note. received no response." },
+		{ "jobPosition": "position 3", "companyName": "Net name3 ", "addedBy": username, "dateApplied": makeDate(-52), "resources": myResources, "comments": "sent thank you note. received no response." },
+		{ "jobPosition": "position 4", "companyName": "Carter",  "addedBy": username, "dateApplied": makeDate(-200), "resources": myResources, "comments": "Sent references. Waiting on reply." },
 	];
 
 
--- server/controllers/resource.js
+++ server/controllers/resource.js
@@ -1,12 +1,18 @@
 let Resource = require('../models/resource');
 
+const makeDate = (daysAdjustment) => {
+	let currentTime = new Date();
+	currentTime.setDate(currentTime.getDate()+daysAdjustment);
+	return currentTime;
+}
+
 module.exports.all = (req, res) => {
 
  	const loremipsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed gravida est sit amet mi egestas, a pharetra sem hendrerit. Ut sit amet lacinia ex, vel pellentesque metus. In placerat, lacus eget porttitor imperdiet, sem nibh faucibus turpis, ultricies ultricies turpis orci in augue. Integer ut posuere ante. Pellentesque blandit purus at tortor malesuada porttitor venenatis sed lacus.";
  	let myResources = [
-    { "image": "https://www.sololearn.com/Icons/Courses/1024.png", "addedBy":"roxroy", "url": "https://www.website1.com/", "name": "abcd", "dateAdded": "01/03/2016", "rating": "1/5", "golds": "1", "description": loremipsum },
-    { "image": "https://image.flaticon.com/teams/new/1-freepik.jpg", "addedBy":"CClar", "url": "https://www.website2.com/", "name": "aaab", "dateAdded": "02/14/2017", "rating": "2/5", "golds": "2", "description": loremipsum },
-    { "image": "http://www.freeiconspng.com/uploads/flat-mac-icon-15.png", "addedBy":"icartusacrimea", "url": "https://www.website3.com/", "name": "accc", "dateAdded": "01/01/2017", "rating": "3/5", "golds": "3", "description": loremipsum }
+    { "image": "https://www.sololearn.com/Icons/Courses/1024.png", "addedBy":"roxroy", "url": "https://www.website1.com/", "name": "abcd", "dateAdded": makeDate(-2), "rating": "1/5", "golds": "1", "description": loremipsum },
+    { "image": "https://image.flaticon.com/teams/new/1-freepik.jpg", "addedBy":"CClar", "url": "https://www.website2.com/", "name": "aaab", "dateAdded": makeDate(-162), "rating": "2/5", "golds": "2", "description": loremipsum },
+    { "image": "http://www.freeiconspng.com/uploads/flat-mac-icon-15.png", "addedBy":"icartusacrimea", "url": "https://www.website3.com/", "name": "accc", "dateAdded": makeDate(-31), "rating": "3/5", "golds": "3", "description": loremipsum }
  	];
 
 	Resource.find({}, function(err, resources) {
