b2c48759859ac922e253ea4c3f02fe30ae537b36
icartusacrimea <icartusacrimea@gmail.com>
2017-06-14 21:02:10 -0400

    changes made two days ago

--- client/src/ResourceRow.js
+++ client/src/ResourceRow.js
@@ -10,7 +10,7 @@ class ResourceRow extends Component {
   }    
   handleClick(){
     this.props.handleResourceModal(this.props.row);
-    return;
+    //return;
   }
   delete(){
     //let deleteName = this.props.row.name,
