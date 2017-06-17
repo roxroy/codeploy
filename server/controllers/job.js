91a5523a57615245beac1b2137e1393253892f9f
Unknown <roxroy@devicode.com>
2017-06-17 09:06:40 -0700

    Removes test data.

--- server/controllers/job.js
+++ server/controllers/job.js
@@ -6,35 +6,32 @@ const makeDate = (daysAdjustment) => {
 	return currentTime;
 }
 
-module.exports.all = (req, res) => {
-	let username = "";
-	if (req.user)
-		username = req.user.username;
-
-
-	const myResources = [
-		{ "image": "https://www.sololearn.com/Icons/Courses/1024.png", "url": "https://www.website1.com/", "addedBy": "user1", "name": "abcd", "date": makeDate(-2), "rating": "1/5", "golds": "1" },
-		{ "image": "https://www.sololearn.com/Icons/Courses/1024.png", "url": "https://www.website2.com/", "addedBy": "user2", "name": "aaab", "date": makeDate(-100), "rating": "2/5", "golds": "2" },
-		{ "image": "https://www.sololearn.com/Icons/Courses/1024.png", "url": "https://www.website3.com/", "addedBy": "user3", "name": "name3", "date": makeDate(-80), "rating": "3/5", "golds": "3" },
-	];
-
-	Jobs.find({addedBy : username }, function(err, jobs) {
-	  if (err) throw err;
-
-	  jobs.forEach( item => {
-	  	myJobs.push({
+const mapItem = (item) => {
+	return {
 	  		ID : item._id,
 	  		jobPosition :  item.jobPosition,
 	  		companyName: item.companyName,
 	  		dateApplied:  item.dateApplied,
 	  		addedBy:  item.addedBy,
-	  		resources: myResources,
+	  		resources: item.resources,
 	  		comments: item.comments,
-	  	});
+	}
+}
+
+module.exports.all = (req, res) => {
+	let username = "";
+	if (req.user)
+		username = req.user.username;
+
+	Jobs.find({addedBy : username }, function(err, jobs) {
+	  if (err) throw err;
+
+	  let myJobs = [];
+	  jobs.forEach( item => {
+	  	myJobs.push(mapItem(item));
 	  });
 	  
-	  jobs = myJobs;
-		res.status(200).send(jobs);
+		res.status(200).send(myJobs);
 	});
 };
 
